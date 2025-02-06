ae8f40b77cbedc9354d4ffe546464345343f9996
Jacobson-svg <jacobfotso1714@gmail.com>
2025-02-06 13:06:10 +0100

    Some changes

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client';
-import App from "./App"
+// import App from "./App"
+
+function App(){
+
+    return("Hello")
+}
 
 ReactDOM
     .createRoot(document.getElementById('root'))
